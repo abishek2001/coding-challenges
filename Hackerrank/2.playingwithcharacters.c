26b67544482c9c3e867446c8e8d518a4ce102abf
bhavna <62351745+bhavnarao@users.noreply.github.com>
2020-11-21 19:15:42 +0530

    updated playingwithcharacters.c

--- Hackerrank/2.playingwithcharacters.c
+++ Hackerrank/2.playingwithcharacters.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
 #include <stdlib.h>
+
 int main() 
 {
     char ch;
-    char s[20],sen[100];
-scanf("%c", &ch );
-scanf("%s",&s);
-scanf("\n");
-scanf("%[^\n]%*c",&sen);
-printf("%c\n",ch);
-printf("%s\n",s);
-printf("%s\n",sen);
+    char s[20],sen[20];
+    scanf("%c",&ch);
+    scanf("%s\n",&s);
+    scanf("%[^\n]%*c",&sen);
+    printf("%c\n",ch);
+    printf("%s\n",s);
+    printf("%s",sen);
+    
+    
     return 0;
 }
